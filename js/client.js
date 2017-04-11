c75267eda947e44cf459bfb32499a18adf6fb53c
GaganTut <gstut@live.com>
2017-04-11 10:56:45 -1000

    Can grab data and display it

--- js/client.js
+++ js/client.js
@@ -5,13 +5,51 @@ function createRequest(apiLink, eventListener) {
   weatherReq.open('GET', apiLink);
   weatherReq.send();
 }
-
 function runRequest() {
   let requestData = JSON.parse(this.responseText);
   console.log(requestData);
+  displayVisual(requestData);
+}
+//const API_CALL = `http://api.openweathermap.org/data/2.5/forecast?q=${cityName},${countryCode}&APPID=${API_KEY}`;
+
+const cityInput = document.querySelector('#cityInput');
+const inputBtn = document.querySelector('#inputBtn');
+const visualForecastDiv = document.querySelector('#visualForecast');
+
+inputBtn.addEventListener('click', () => {
+  let cityName = cityInput.value;
+  let apiLink = `http://api.openweathermap.org/data/2.5/forecast?q=${cityName},us&APPID=${API_KEY}`;
+  createRequest(apiLink, runRequest);
+});
+
+function displayVisual(cityData) {
+  removeOldForecast();
+  for (let i = 0; i < cityData.list.length; i += 8) {
+    visualForecastDiv.appendChild(createDailyVisual(cityData, cityData.list[i]));
+  }
 }
 
-const API_CALL = `http://api.openweathermap.org/data/2.5/forecast?q=${cityName},${countryCode}&APPID=${API_KEY}`;
+const createDailyVisual = (wholeObj, dayObj) => {
+  let dayDiv = document.createElement('div');
 
+  let cityDataName = document.createElement('h1');
+  cityDataName.innerHTML = wholeObj.city.name;
 
-createRequest(API_CALL, runRequest);
+  let dateVis = document.createElement('h3');
+  dateVis.innerHTML = dayObj.dt_txt;
+
+  let tempVis = document.createElement('h2');
+  tempVis.innerHTML = `Temperature: ${(dayObj.main.temp * 9/5 - 459.67).toFixed(2)} &deg F`;
+
+  dayDiv.appendChild(cityDataName);
+  dayDiv.appendChild(dateVis);
+  dayDiv.appendChild(tempVis);
+
+  return dayDiv;
+};
+
+const removeOldForecast = () => {
+  while (visualForecastDiv.hasChildNodes()) {
+    visualForecastDiv.removeChild(visualForecastDiv.lastChild);
+  }
+};
